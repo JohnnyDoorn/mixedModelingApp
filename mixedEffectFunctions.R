e482a0a8184cd52e63e148fccc7ecd875a943f50
Johnny <johnnydoorn@gmail.com>
2020-10-06 12:40:42 +0200

    Add ggplot

--- mixedEffectFunctions.R
+++ mixedEffectFunctions.R
@@ -35,9 +35,9 @@ myDataSim <- function(fixedEffectItem = 0, randomSlopeItemSigma = 0, randomInter
   randomEffectsCondition <- mvrnorm(n = I, mu = c(0,0) , Sigma = randomEffectMatCondition)
   colnames(randomEffectsItem) <- colnames(randomEffectsCondition) <- c("intercept", "slope")
   
-  y <- intercept + randomEffectsItem[,"intercept"][id] + randomEffectsCondition[,"intercept"][id] +
-    (fixedEffectCondition + randomEffectsCondition[,"slope"][id]) * fixedEffectConditionVector[cond] +
-    (fixedEffectItem + randomEffectsItem[,"slope"][id]) * fixedEffectItemVector[item] + err
+  y <- intercept + randomEffectsItem[, "intercept"][id] + randomEffectsCondition[, "intercept"][id] +
+    (fixedEffectCondition + randomEffectsCondition[, "slope"][id]) * fixedEffectConditionVector[cond] +
+    (fixedEffectItem + randomEffectsItem[, "slope"][id]) * fixedEffectItemVector[item] + err
   
   mydat <- data.frame(y = y,
                       cond = as.factor(cond),
@@ -45,37 +45,3 @@ myDataSim <- function(fixedEffectItem = 0, randomSlopeItemSigma = 0, randomInter
                       id = as.factor(id))
   return(mydat)
 }
-
-
-compute4BF=function(dat){
-  y=dat$y
-  n=length(y)
-  X0=rep(1,n)
-  I <- length(unique(dat$id))
-  Xsub=matrix(0,nrow=n,ncol=I)
-  for (i in 1:n) Xsub[i, dat$id[i]]=1
-  X1 = sign(as.numeric(dat$cond) - 1.5) * (1/sqrt(2))
-  XsubCond=matrix(0,nrow=n,ncol=I)
-  for (i in 1:n) XsubCond[i, dat$id[i]] <- sign(as.numeric(dat$cond[i]) - 3/2) * (1/sqrt(2))
-  
-  bf=1:4
-  #Mod 1 Strong Null, no condition effects
-  X=cbind(X0,Xsub)
-  g=rep(0,I)
-  bf[1]=nWayAOV(y,X,g,rscale=1)$bf
-  #Mod 2 No subject-by-treatment interactions
-  X=cbind(X0,Xsub,X1)
-  g=c(rep(0,I),1)
-  bf[2]=nWayAOV(y,X,g,rscale=c(1,1))$bf
-  #Mod 3  Zero-Centered subject-by-treatment interactions (silly model)
-  X=cbind(X0,Xsub,XsubCond)
-  g=c(rep(0,I),rep(1,I))
-  bf[3]=nWayAOV(y,X,g,rscale=c(1, 1))$bf
-  #Mod 4 Main effect with subject-by-treatment interactions
-  X=cbind(X0,Xsub,X1,XsubCond)
-  g=c(rep(0,I),1,rep(2,I))
-  bf[4]=nWayAOV(y,X,g,rscale=c(1, 1, 1))$bf
-  out=bf
-  names(out)=c("M0","M2", "M3", "M4")
-  return(out)
-}  
