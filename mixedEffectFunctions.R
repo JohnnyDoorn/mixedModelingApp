38105b0e4a37b0d0cdcf7fdacc1e96d8c4f0b4b4
Johnny <johnnydoorn@gmail.com>
2020-10-06 18:10:52 +0200

    add plot function

--- mixedEffectFunctions.R
+++ mixedEffectFunctions.R
@@ -45,3 +45,39 @@ myDataSim <- function(fixedEffectItem = 0, randomSlopeItemSigma = 0, randomInter
                       id = as.factor(id))
   return(mydat)
 }
+
+
+plotBoxplotLines <- function(mydat) {
+  par(mfrow = c(1, 2), cex = 1.2)
+  mydatAgg <- as.data.frame.table(tapply(mydat$y,list(mydat$id,mydat$cond),mean))
+  colnames(mydatAgg)=c("id","cond", "y")
+  
+  mylwd <- 2
+  nCond <- nlevels(mydat$cond)
+  nItem <- nlevels(mydat$item)
+  nSub <- nlevels(mydat$id)
+  palette(rainbow(nSub))
+  allCols <- adjustcolor(palette(), alpha.f = 0.4)
+  plot(mydat$cond, mydat$y, bty = "n", las = 1, type ="n", xlab = "Condition", ylab = "y", main = "Full Data")
+  
+  points(as.numeric(mydat$cond), mydat$y)
+  for (i in 1:nSub) {
+    ss <- subset(mydat, id == i)
+    for (j in 1:nItem) {
+      sss <- subset(ss, item == j)
+      lines(1:nCond, sss$y, col = allCols[i], lwd = mylwd)
+    }
+  }
+  
+  boxplot(mydat$y ~ mydat$cond, bty = "n", las = 1, type ="n", 
+          xlab = "Condition", ylab = "y", main = "Aggregated Data", 
+          col = "white", border = "white", bg = "white")
+  boxplot(mydatAgg$y ~ mydatAgg$cond, add = TRUE, las = 1)
+  points(as.numeric(mydatAgg$cond), mydatAgg$y)
+  for (i in 1:nSub) {
+    ss <- subset(mydatAgg, id == i)
+    lines(1:nCond, tapply(ss$y, ss$cond, mean), col = allCols[i], lwd = mylwd)
+  }
+}
+
+
